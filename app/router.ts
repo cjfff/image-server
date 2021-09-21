cdf8ab3d1d992b5916c0b3de85aee94bbc5963f8
chenjingfeng <chenjingfeng@joyy.sg>
2021-09-21 21:01:31 +0800

    feat: max-listeners

--- app/router.ts
+++ app/router.ts
@@ -1,11 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2021-09-19 11:46:27
- * @LastEditTime: 2021-09-19 11:57:17
- * @LastEditors: your name
+ * @LastEditTime: 2021-09-21 20:55:54
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /egg-poster-generator-server/app/router.ts
  */
+process.setMaxListeners(0);
+
 import { Application } from 'egg';
 
 export default (app: Application) => {
